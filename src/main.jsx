8d2f13d8b38c969a2061ffff1464e68da0ffff61
TriTruong666 <tritruonghoang3@gmail.com>
2024-09-12 23:23:58 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import route
 import router from "./routes/combineRoutes";
 // import styles
 import "./styles/global/global.css";
 // import redux
 import store from "./redux/store";
+const cilent = new QueryClient({});
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <QueryClientProvider cilent={cilent}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </Provider>
 );

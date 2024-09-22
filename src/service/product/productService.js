8916f5f99b40aaa538e751bfd21cf6cf76f0c981
thanhdicode <thanhhdse184021@fpt.edu.vn>
2024-09-22 10:25:45 +0700

    getAllproducts

--- src/service/product/productService.js
+++ src/service/product/productService.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
-const token = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkZW1vLmNvbSIsInN1YiI6ImFkbWluIiwiZXhwIjoxNzI2OTM5MzQzLCJpYXQiOjE3MjY5MzU3NDMsImp0aSI6ImE5N2Y0YzMzLTZhMDUtNDkwMS1iMWI0LWJlMjM4ZmY4YjUxYSIsInNjb3BlIjoiQURNSU4ifQ.OuE6ujXV3Q7QMY5Hyo9CEz3zGTYsKLbQ9QbaN-H5EP38_cWAm72MEQfT8dFkTPkr1c6SGydVzh_OLWy3q-c9_g";
 
 export const getAllProduct = async () => {
   try {
+    // Retrieve the token from localStorage
+    const token = localStorage.getItem("token");
+
+    // If token doesn't exist
+    if (!token) {
+      throw new Error('No authentication token found');
+    }
     const response = await axios.get('http://localhost:8080/product/list', {
       headers: {
         Authorization: `Bearer ${token}`, // Attach token to Authorization header

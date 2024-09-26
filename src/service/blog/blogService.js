2f4172daa54c4d39ecbb638e6fa40ddea5f710f5
thanhdicode <thanhhdse184021@fpt.edu.vn>
2024-09-26 13:11:08 +0700

    blog service

---
+++ src/service/blog/blogService.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+export const getAllBlog = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/blog/list", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data.result;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const createProductService = async (blogData) => {
+  const token = localStorage.getItem("token");
+  try {
+    const api = "http://localhost:8080/blog/create";
+    const res = await axios.post(api, blogData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export const editBlog = async (blogId, updatedBlogData) => {
+  const token = localStorage.getItem("token");
+  try {
+    const api = `http://localhost:8080/blog/edit/${blogId}`;
+    const res = await axios.put(api, updatedBlogData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}; 

6ff65a4c0596cf752eb90c6fd2708f8cfc16da28
tritruong666 <tritruonghoang3@gmail.com>
2024-09-30 22:51:28 +0700

    fix comments

--- src/service/blog/blogService.js
+++ src/service/blog/blogService.js
@@ -75,12 +75,6 @@ export const detailBlogService = async (blogId) => {
   }
 };
 
-/**
- * Deletes a blog post by its ID.
- *
- * @param {int} blogId - The unique ID of the blog post to delete.
- * @returns {object} The response data from the API.
- */
 export const deleteBlogService = async (blogId) => {
   try {
     const api = `http://localhost:8080/blog/delete/${blogId}`;

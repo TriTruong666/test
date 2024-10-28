c1f54d74a23b3ff9fb0d356d1baa2fb38009dd8e
thanhdicode <thanhhdse184021@fpt.edu.vn>
2024-10-28 16:06:52 +0700

    fix

--- src/pages/private/member/HomeMember.jsx
+++ src/pages/private/member/HomeMember.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
 import member from "../../../assets/kois.png";
 //charts
 import { SizeAndWeightChart } from "../../../chart/member/SizeAndWeightChart";
@@ -9,6 +10,7 @@ import { Dashnav } from "../../../components/navbar/Dashnav";
 import * as BlogService from "../../../service/blog/blogService";
 import * as OrderService from "../../../service/order/order";
 import * as PondService from "../../../service/pond/pondService";
+import * as ProductService from "../../../service/product/productService";
 //styles
 import "../../../styles/dashboard/home/home.css";
 
@@ -54,6 +56,25 @@ export const HomeMember = () => {
     queryKey: ["member-ponds", userId],
     queryFn: () => PondService.getUserPondService(userId),
     refetchOnWindowFocus: false,
+  });
+
+  const { data: products = [] } = useQuery({
+    queryKey: ["member-products", userId],
+    queryFn: ProductService.getAllProductShop,
+  });
+
+  const pondStatus = ponds.some((pond) => {
+    const status = localStorage.getItem(`pondStatus-${pond.pondId}`);
+    return status === "poor" || status === "moderate";
+  });
+
+  // Determine recommended products based on pond status
+  const recommendedProducts = products.filter((product) => {
+    if (!pondStatus) {
+      return product.category.cateName === "Koi Health Treatment";
+    } else {
+      return product.category.cateName === "Water Parameter Improvement";
+    }
   });
 
   const handleOrderStatusClassName = (status) =>
@@ -116,117 +137,163 @@ export const HomeMember = () => {
     <div className="homemem-dashboard-container">
       <Dashnav />
       <div className="dashboard">
-        <div className="dashboard-header">
-          <strong>Home Dashboard</strong>
-        </div>
-        <div className="section1">
-          <div className="item">
-            <div className="item1">
-              <strong>Welcome back {user.fullname}</strong>
-              <p>IZUMIYA is the Simplest Manage System of Koi</p>
-            </div>
-            <div className="item2">
-              <img src={member} alt="Member" />
-            </div>
+        {serverError ? (
+          <div className="error-page">
+            <p>{serverError}</p>
           </div>
-
-          <div className="info">
-            <div className="small-item">
-              <div>
-                <strong>{blogs.length} blogs</strong>
-                <p> Contributed</p>
+        ) : isLoadingPage ? (
+          <div className="loading">
+            <ClipLoader color="#000000" size={40} />
+          </div>
+        ) : (
+          <>
+            <div className="dashboard-header">
+              <strong>Home Dashboard</strong>
+            </div>
+            <div className="section1">
+              <div className="item">
+                <div className="item1">
+                  <strong>Welcome back {user.fullname}</strong>
+                  <p>IZUMIYA is the Simplest Manage System of Koi</p>
+                </div>
+                <div className="item2">
+                  <img src={member} alt="Member" />
+                </div>
               </div>
-              <i className="bx bx-news"></i>
-            </div>
-            <div className="small-item">
-              <div>
-                <strong>{orders.length}</strong>
-                <p>Total orders</p>
+
+              <div className="info">
+                <div className="small-item">
+                  <div>
+                    <strong>{blogs.length} blogs</strong>
+                    <p> Contributed</p>
+                  </div>
+                  <i className="bx bx-news"></i>
+                </div>
+                <div className="small-item">
+                  <div>
+                    <strong>{orders.length}</strong>
+                    <p>Total orders</p>
+                  </div>
+                  <i className="bx bx-package"></i>
+                </div>
               </div>
-              <i className="bx bx-package"></i>
             </div>
-          </div>
-        </div>
 
-        <div className="chart-container">
-          <div className="charts">
-            <div className="left-chart">
-              <h3>The ponds charts</h3>
+            <div className="chart-container">
+              <div className="charts">
+                <div className="left-chart">
+                  <h3>Recommend Products for your ponds</h3>
+                  {recommendedProducts.length === 0 ? (
+                    <p>You have not create any ponds</p>
+                  ) : (
+                    <ul className="recommended-products-list">
+                      {recommendedProducts.slice(0, 3).map((product) => (
+                        <li
+                          key={product.productId}
+                          className="recommended-product-item"
+                        >
+                          <img
+                            src={product.image}
+                            alt={product.productName}
+                            className="recommended-product-image"
+                          />
+                          <div className="recommended-product-details">
+                            <strong>{product.productName}</strong>
+                            <p>{formatPrice(product.unitPrice)}</p>
+                            <Link to={`/productdetail/${product.productId}`}>
+                              Details
+                            </Link>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+                <div className="right-chart">
+                  <h3>Koi Growth and Weight Logs </h3>
+                  <SizeAndWeightChart koiGrowthLogs={koiLogs} />
+                </div>
+              </div>
             </div>
-            <div className="right-chart">
-              <h3>Koi Growth and Weight Logs {pondLabel}</h3>
-              <SizeAndWeightChart koiGrowthLogs={koiLogs} />
-            </div>
-          </div>
-        </div>
-        <div className="summary-bottom">
-          <div className="left-bottom">
-            <h3>Recent Orders</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>OrderId</th>
-                  <th>Total items</th>
-                  <th>Price</th>
-                  <th>Status</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {orders.map((order) => (
-                  <tr key={order.order.orderId}>
-                    <td>{order.order.orderId}</td>
-                    <td>{order.order.orderDetails.length} </td>
-                    <td>{formatPrice(order.order.total)}</td>
-                    <td>
-                      <span
-                        className={handleOrderStatusClassName(
-                          order.order.status
-                        )}
-                      >
-                        {handleStatusTitle(order.order.status)}
-                      </span>
-                    </td>
-                    <td>
-                      <Link
-                        to={`/dashboard/myorder/detail/${order.order.orderId}`}
-                      >
-                        Details
-                      </Link>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          <div className="right-bottom">
-            <h3>Pond Information</h3>
-            <table>
-              <thead>
-                <tr>
-                  <th>Pond Id</th>
-                  <th>Pond Name</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {ponds.map((pond) => {
-                  const pondStatus = localStorage.getItem(
-                    `pondStatus-${pond.pondId}`
-                  );
-                  return (
-                    <tr key={pond.pondId}>
-                      <td>{pond.pondId}</td>
-                      <td>{pond.pondName}</td>
-                      <td className={pondStatus}>{pondStatus}</td>
+            <div className="summary-bottom">
+              <div className="left-bottom">
+                <h3>Recent Orders</h3>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>OrderId</th>
+                      <th>Total items</th>
+                      <th>Price</th>
+                      <th>Status</th>
+                      <th>Action</th>
                     </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
+                  </thead>
+                  <tbody>
+                    {orders.map((order) => (
+                      <tr key={order.order.orderId}>
+                        <td>{order.order.orderId}</td>
+                        <td>{order.order.orderDetails.length} </td>
+                        <td>{formatPrice(order.order.total)}</td>
+                        <td>
+                          <span
+                            className={handleOrderStatusClassName(
+                              order.order.status
+                            )}
+                          >
+                            {handleStatusTitle(order.order.status)}
+                          </span>
+                        </td>
+                        <td>
+                          <Link
+                            to={`/dashboard/myorder/detail/${order.order.orderId}`}
+                          >
+                            Details
+                          </Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+
+              <div className="right-bottom">
+                {ponds.length === 0 ? (
+                  <>
+                    <h3>Pond Information</h3>
+                    <p>You have not created any ponds.</p>
+                  </>
+                ) : (
+                  <div className="ponds-info">
+                    <h3>Pond Information</h3>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Pond Id</th>
+                          <th>Pond Name</th>
+                          <th>Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {ponds.map((pond) => {
+                          const pondStatus = localStorage.getItem(
+                            `pondStatus-${pond.pondId}`
+                          );
+                          return (
+                            <tr key={pond.pondId}>
+                              <td>{pond.pondId}</td>
+                              <td>{pond.pondName}</td>
+                              <td className={pondStatus}>{pondStatus}</td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                )}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
